718b3a210136903770272ff3ad9ed16cdf15f25c
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-09-19 21:50:09 -0700

    added comments to mergeSortedArray

--- ds/array/mergeSortedArray.js
+++ ds/array/mergeSortedArray.js
@@ -1,4 +1,5 @@
 function mergeSortedArrays(arr1, arr2) {
+    // check that you received arrays
     if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
         return 'invalid input'
     }
@@ -7,6 +8,7 @@ function mergeSortedArrays(arr1, arr2) {
     let arr1Pointer = 0;
     let arr2Pointer = 0;
 
+    // while both arrays have items left check for lower num and push to mergedArray, increment the lower arrays pointer
     while (arr1Pointer < arr1.length && arr2Pointer < arr2.length) {
         const arr1Val = arr1[arr1Pointer];
         const arr2Val = arr2[arr2Pointer];
@@ -20,6 +22,7 @@ function mergeSortedArrays(arr1, arr2) {
         }
     }
 
+    // check if either array has items left, if so we know they are in order so spread items into mergedArray
     if (arr1Pointer < arr1.length) {
         mergedArray.push(...arr1.slice(arr1Pointer))
     }
