32c38323030509bf16d3214d68fda5be98fc0b61
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-09-26 20:46:28 -0700

    implemented stack with linked list and array js

---
+++ ds/stack/array/stack.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    peek() {
+        return this.array[this.array.length - 1];
+    }
+
+    push(value) {
+        this.array.push(value);
+        return this;
+    }
+
+    pop() {
+        const poppedValue = this.array.pop();
+        return poppedValue;
+    }
+}
+
+const stack = new Stack();
+stack.push('google');
+stack.push('udemy')
+stack.push('discord')
+console.log(stack)
+stack.pop();
+stack.pop()
+console.log(stack.pop())
+console.log(stack)
---
+++ ds/stack/linked-list/stack.js
@@ -0,0 +1,56 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        return this.top;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.length) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        if (this.top === this.bottom) {
+            this.bottom = null
+        }
+
+        const holdingPointer = this.top;
+        this.top = holdingPointer.next;
+        this.length--;
+        return holdingPointer.value;
+    }
+};
+
+const stack = new Stack();
+stack.push('google');
+stack.push('udemy')
+stack.push('discord')
+stack.pop();
+stack.pop()
+console.log(stack.pop())
+console.log(stack)
